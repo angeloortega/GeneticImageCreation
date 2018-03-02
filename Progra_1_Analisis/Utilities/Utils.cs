c0984544d9bb44da0911b7e71808c17e19d4e510
angeloortega <angeloale1@gmail.com>
2018-03-01 19:48:57 -0600

    Image resizing and squaring

--- Progra_1_Analisis/Utilities/Utils.cs
+++ Progra_1_Analisis/Utilities/Utils.cs
@@ -38,7 +38,6 @@ namespace Progra_1_Analisis.Utilities
                     }
                 }
                 poblacion[cantidad - 1] = new Imagen("imagen" + cantidad, bmp);
-                bmp.Save("C:\\Users\\Angelo PC\\Desktop\\unPichazoDeImagenes\\imagen" +cantidad + ".png", ImageFormat.Png);
                 cantidad--;
             }
             singleton.poblacion = poblacion;
@@ -51,13 +50,36 @@ namespace Progra_1_Analisis.Utilities
         
             if (open.ShowDialog() == DialogResult.OK)
             {
-                Bitmap bmp = new Bitmap(open.FileName);
+                Bitmap resizedImage = PadImage(resizeImage(128, 128, new Bitmap(open.FileName)));
                 SingletonCache singleton = SingletonCache.Instance;
-                singleton.objetivo = new Imagen("objetivo", bmp);
+                singleton.objetivo = new Imagen("objetivo", resizedImage);
                 // display image in picture box  
-                return bmp;
+                return resizedImage;
             }
             return null;
+        }
+        public static Bitmap resizeImage(int finalHeight, int finalWidth, Bitmap original) {
+            float scaleHeight = finalHeight / (float)original.Height;
+            float scaleWidth = finalWidth / (float)original.Width;
+            float scale = Math.Min(scaleHeight, scaleWidth);
+            return new Bitmap(original, (int)(original.Width * scale), (int)(original.Height * scale));
+        }
+
+        public static Bitmap PadImage(Bitmap originalImage)
+        {
+            int largestDimension = Math.Max(originalImage.Height, originalImage.Width);
+            Size squareSize = new Size(largestDimension, largestDimension);
+            Bitmap squareImage = new Bitmap(squareSize.Width, squareSize.Height);
+            using (Graphics graphics = Graphics.FromImage(squareImage))
+            {
+                graphics.FillRectangle(Brushes.Transparent, 0, 0, squareSize.Width, squareSize.Height);
+                graphics.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
+                graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+
+                graphics.DrawImage(originalImage, (squareSize.Width / 2) - (originalImage.Width / 2), (squareSize.Height / 2) - (originalImage.Height / 2), originalImage.Width, originalImage.Height);
+            }
+            return squareImage;
         }
     }
 }
